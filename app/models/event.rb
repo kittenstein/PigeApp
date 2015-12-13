911fc7172dfa4049729926b2d942950ee52491d0
drobert19 <dumitru_robert19@yahoo.com>
2015-12-13 10:12:35 +0200

    Add all files

---
+++ app/models/event.rb
@@ -0,0 +1,7 @@
+class Event < ActiveRecord::Base
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :header, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 140 }
+end
